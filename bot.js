b6ced26cf5a3338c3f1be3611e4e953aa8527ec2
VanGans <41531599+Lamer1337Crew@users.noreply.github.com>
2020-08-30 16:42:49 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ var query = require('samp-query');
 //@audit Settings
 
 const botChar = "/"; // Bot prefix character
-let Samp_IP = "51.178.138.254";
+let Samp_IP = "52.187.147.73";
 let Samp_Port = 7777;
 let Community_Tag ="WG";
 
